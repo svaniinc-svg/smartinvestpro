981b4b665d583705cbcf0995a6386b81f5feb641
svaniinc-svg <svaniinc@gmail.com>
2025-10-26 09:23:53 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -8,16 +8,6 @@ from openpyxl import load_workbook
 from openpyxl.styles import Font, PatternFill
 import matplotlib.pyplot as plt
 
-import streamlit as st
-st.set_page_config(
-    page_title="Real Estate ROI Calculator",
-    layout="wide",
-    menu_items={
-        "Get Help": None,
-        "Report a bug": None,
-        "About": None
-    }
-)
 
 
 st.set_page_config(page_title="Real Estate ROI Calculator", layout="wide")
@@ -849,4 +839,5 @@ if st.button("Calculate"):
         file_name="roi_amortization.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
