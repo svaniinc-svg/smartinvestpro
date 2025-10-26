a4af7b79f35cd82b357ff33148f40509a7591ae6
svaniinc-svg <svaniinc@gmail.com>
2025-10-26 09:22:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ from openpyxl import load_workbook
 from openpyxl.styles import Font, PatternFill
 import matplotlib.pyplot as plt
 
+import streamlit as st
+st.set_page_config(
+    page_title="Real Estate ROI Calculator",
+    layout="wide",
+    menu_items={
+        "Get Help": None,
+        "Report a bug": None,
+        "About": None
+    }
+)
+
+
 st.set_page_config(page_title="Real Estate ROI Calculator", layout="wide")
 st.title("🏡 Real Estate ROI Calculator with Multiple Units")
 
@@ -837,3 +849,4 @@ if st.button("Calculate"):
         file_name="roi_amortization.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
